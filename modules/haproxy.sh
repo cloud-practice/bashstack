a790db77477339d736459439554915f77451f1e7
Jon Jozwiak <jjozwiak@redhat.com>
2015-05-14 09:46:01 -0500

    Updating some haproxy

--- modules/haproxy.sh
+++ modules/haproxy.sh
@@ -3,6 +3,10 @@
 # Module:	haproxy
 # Description:	Install haproxy Load Balancer
 ##########################################################################
+
+# TODO: haproxy.cfg
+
+
 ANSWERS=/root/bashstack/answers.txt
 
 if [[ ! -f $ANSWERS ]] ; then
@@ -10,4 +14,19 @@ if [[ ! -f $ANSWERS ]] ; then
 else
   source $ANSWERS
 fi
+
+yum -y install haproxy 
+echo net.ipv4.ip_nonlocal_bind=1 >> /etc/sysctl.d/haproxy.conf
+echo 1 > /proc/sys/net/ipv4/ip_nonlocal_bind
+# the keepalive settings must be set in *ALL* hosts interacting with rabbitmq.
+cat >/etc/sysctl.d/tcp_keepalive.conf << EOF
+net.ipv4.tcp_keepalive_intvl = 1
+net.ipv4.tcp_keepalive_probes = 5
+net.ipv4.tcp_keepalive_time = 5
+EOF
+sysctl net.ipv4.tcp_keepalive_intvl=1
+sysctl net.ipv4.tcp_keepalive_probes=5
+sysctl net.ipv4.tcp_keepalive_time=5
+
+
 
