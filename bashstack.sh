709ff4cd8ae366dd405723a283496389822f7f6c
Jon Jozwiak <jjozwiak@redhat.com>
2015-05-15 06:38:26 -0500

    Adding some comments to bashstack.sh

--- bashstack.sh
+++ bashstack.sh
@@ -6,3 +6,6 @@
 
 getopt -al -- $@
 
+# --dry-run  - Executes a dry run and outputs every command to be run 
+# --dry-run-no-ssh - Same as dry run, but executed locally (needed?) 
+# --gen-answer-file (=file) - Creates the answers file
